5da9ea442709dd7bfc0fc1d37287dc8128b9cac1
arzoo.shaikh <arzoo.rkcet@gmail.com>
2024-07-26 10:29:34 +0530

    update with login logic

--- ecom_app/lib/screens/login_screen.dart
+++ ecom_app/lib/screens/login_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   TextEditingController txtUname = TextEditingController();
+
   TextEditingController txtPwd = TextEditingController();
+
+  bool isHidden = true;
+  String msg = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,18 +34,38 @@ class LoginScreen extends StatelessWidget {
         ),
         TextField(
           controller: txtPwd,
-          obscureText: true,
+          obscureText: isHidden,
           decoration: InputDecoration(
             labelText: 'Password',
             hintText: 'Enter your password',
+            suffixIcon: IconButton(
+              onPressed: () {
+                //to change visibility of password field
+                // if(isHidden)isHidden=false;else isHidden= true;
+                isHidden = !isHidden;
+                print('isHidden $isHidden');
+                setState(() {});
+              },
+              icon: Icon(isHidden ? Icons.visibility : Icons.visibility_off),
+            ),
           ),
         ),
         ElevatedButton(
           onPressed: () {
             print('from login button clicked ${txtUname.text} ${txtPwd.text}');
+            if (txtUname.text.isEmpty || txtPwd.text.isEmpty) {
+              msg = 'Enter details in all fields';
+            } else if (txtUname.text == txtPwd.text) {
+              msg = 'welceome ${txtUname.text}';
+            } else {
+              msg = 'Invalid credentials';
+            }
+            print(msg);
+            setState(() {});
           },
           child: Text('Login'),
         ),
+        Text(msg),
       ],
     ));
   }
